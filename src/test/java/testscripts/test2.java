8e9b2a969e3f427f45b1d13966798c12be549f9e
ozbuddy <ozbuddy9@gmail.com>
2024-04-10 12:42:00 +1000

    new file by x

---
+++ src/test/java/testscripts/test2.java
@@ -0,0 +1,9 @@
+package testscripts;
+
+public class test2 {
+	public static void main(String[] args) {
+		System.out.println("Hello1");
+		System.out.println("Hello2");
+		System.out.println("Hello3");
+	}
+}
